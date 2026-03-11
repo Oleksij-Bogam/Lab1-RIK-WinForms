9b4b2b75925988c8a538c9e9272befb1227221fa
Alex <Alex@DESKTOP-N9S9SHU>
2026-03-11 12:11:51 +0200

    Add project files.

---
+++ Lab1_Survey_WinForms/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Lab1_Survey_WinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Location = new System.Drawing.Point(9, 21);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(35, 13);
+            this.lblQuestion.TabIndex = 0;
+            this.lblQuestion.Text = "label1";
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(12, 54);
+            this.txtAnswer.Multiline = true;
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(253, 168);
+            this.txtAnswer.TabIndex = 1;
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(12, 228);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(75, 23);
+            this.btnPrev.TabIndex = 2;
+            this.btnPrev.Text = "Back";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(190, 228);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(190, 257);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save txt";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(93, 285);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(35, 13);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(277, 307);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.lblQuestion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ Lab1_Survey_WinForms/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lab1_Survey_WinForms
+{
+    public partial class Form1 : Form
+    {
+        private List<string> _questions = new List<string>
+        {
+            "1) Як вас звати?",
+            "2) Скільки вам років?",
+            "3) Які цілі на цей семестр?",
+            "4) Скільки лабораторних вже зробили?",
+            "5) Який прогрес з дипломом?"
+        };
+
+        private Dictionary<int, string> _answers = new Dictionary<int, string>();
+        private int _index = 0;
+
+        private string _outputFilePath = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            txtAnswer.Multiline = true;
+            txtAnswer.ScrollBars = ScrollBars.Vertical;
+
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
+            lblQuestion.Text = _questions[_index];
+
+            string a;
+            txtAnswer.Text = _answers.TryGetValue(_index, out a) ? a : "";
+
+            btnPrev.Enabled = _index > 0;
+            btnNext.Enabled = _index < _questions.Count - 1;
+
+            lblStatus.Text = "Питання " + (_index + 1) + " з " + _questions.Count;
+        }
+
+        private void SaveCurrentAnswer()
+        {
+            _answers[_index] = (txtAnswer.Text ?? "").Trim();
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            SaveCurrentAnswer();
+            if (_index > 0) _index--;
+            UpdateUI();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            SaveCurrentAnswer();
+            if (_index < _questions.Count - 1) _index++;
+            UpdateUI();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveCurrentAnswer();
+
+            if (string.IsNullOrWhiteSpace(_outputFilePath))
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Title = "Куди зберегти відповіді?";
+                sfd.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                sfd.FileName = "survey_answers.txt";
+
+                if (sfd.ShowDialog() != DialogResult.OK)
+                {
+                    lblStatus.Text = "Збереження скасовано.";
+                    return;
+                }
+
+                _outputFilePath = sfd.FileName;
+            }
+
+            try
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine("=== Опитування ===");
+                sb.AppendLine("Дата/час: " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+                sb.AppendLine();
+
+                for (int i = 0; i < _questions.Count; i++)
+                {
+                    string ans;
+                    if (!_answers.TryGetValue(i, out ans)) ans = "";
+
+                    sb.AppendLine(_questions[i]);
+                    sb.AppendLine("Відповідь: " + ans);
+                    sb.AppendLine(new string('-', 40));
+                }
+
+                sb.AppendLine();
+                sb.AppendLine("=== Кінець запису ===");
+                sb.AppendLine();
+
+                File.AppendAllText(_outputFilePath, sb.ToString(), Encoding.UTF8);
+
+                lblStatus.Text = "Збережено: " + _outputFilePath;
+                MessageBox.Show("Відповіді збережено успішно!", "OK",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка збереження: " + ex.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+    }
+}
